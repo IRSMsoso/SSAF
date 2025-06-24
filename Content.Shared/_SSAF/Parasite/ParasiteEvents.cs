93d11857aedde9606073862cb432ffc5e7fee4be
Cameron VanderTuig <cameronvandertuig@proton.me>
2025-06-24 00:49:24 -0400

    More complex action

--- Content.Shared/_SSAF/Parasite/ParasiteEvents.cs
+++ Content.Shared/_SSAF/Parasite/ParasiteEvents.cs
@@ -1,4 +1,6 @@
 using Content.Shared.Actions;
+using Content.Shared.DoAfter;
+using Robust.Shared.Serialization;
 
 namespace Content.Shared._SSAF.Parasite;
 
@@ -6,3 +8,6 @@ namespace Content.Shared._SSAF.Parasite;
 public sealed partial class ParasiteInfectHostActionEvent : EntityTargetActionEvent
 {
 }
+
+[Serializable, NetSerializable]
+public sealed partial class InfectHostDoAfterEvent : SimpleDoAfterEvent { }
