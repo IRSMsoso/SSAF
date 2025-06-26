db7bce08707d462667d51f0b22caac6512a29c81
Cameron VanderTuig <cameronvandertuig@proton.me>
2025-06-26 08:40:28 -0400

    More emotions stuff + intro music

--- Content.Server/_SSAF/Parasite/ParasiteSystem.cs
+++ Content.Server/_SSAF/Parasite/ParasiteSystem.cs
@@ -1,11 +1,13 @@
 using Content.Server._SSAF.Parasite.Components;
 using Content.Server.Body.Components;
 using Content.Server.Body.Systems;
+using Content.Server.Chat.Managers;
 using Content.Server.Chat.Systems;
 using Content.Server.DoAfter;
 using Content.Server.Drunk;
 using Content.Server.Jittering;
 using Content.Server.Medical;
+using Content.Server.Mind;
 using Content.Server.Popups;
 using Content.Server.Speech.EntitySystems;
 using Content.Server.Store.Systems;
@@ -14,6 +16,7 @@ using Content.Shared._Shitmed.Targeting;
 using Content.Shared._SSAF.Parasite;
 using Content.Shared.Actions;
 using Content.Shared.Body.Systems;
+using Content.Shared.Chat;
 using Content.Shared.Chat.Prototypes;
 using Content.Shared.Coordinates;
 using Content.Shared.Damage;
@@ -21,6 +24,7 @@ using Content.Shared.DoAfter;
 using Content.Shared.FixedPoint;
 using Content.Shared.Gibbing.Events;
 using Content.Shared.Gibbing.Systems;
+using Content.Shared.Mind;
 using Content.Shared.Movement.Systems;
 using Content.Shared.Nutrition.Components;
 using Content.Shared.Nutrition.EntitySystems;
@@ -29,6 +33,7 @@ using Content.Shared.Store.Components;
 using Robust.Server.Containers;
 using Robust.Shared.Audio.Systems;
 using Robust.Shared.Containers;
+using Robust.Shared.Player;
 using Robust.Shared.Prototypes;
 using Robust.Shared.Random;
 using Robust.Shared.Timing;
@@ -57,6 +62,7 @@ public sealed class ParasiteSystem : EntitySystem
     [Dependency] private readonly ContainerSystem _container = default!;
     [Dependency] private readonly DrunkSpikeSystem _drunkSpike = default!;
     [Dependency] private readonly ChatSystem _chat = default!;
+    [Dependency] private readonly IChatManager _chatManager = default!;
     [Dependency] private readonly DamageableSystem _damageable = default!;
     [Dependency] private readonly BloodstreamSystem _bloodstream = default!;
     [Dependency] private readonly SharedAudioSystem _audio = default!;
@@ -69,6 +75,9 @@ public sealed class ParasiteSystem : EntitySystem
     [Dependency] private readonly JitteringSystem _jittering = default!;
     [Dependency] private readonly GibbingSystem _gibbing = default!;
     [Dependency] private readonly StutteringSystem _stuttering = default!;
+    [Dependency] private readonly MindSystem _mind = default!;
+    [Dependency] private readonly ISharedPlayerManager _player = default!;
+
 
 
     /// <inheritdoc/>
@@ -97,6 +106,13 @@ public sealed class ParasiteSystem : EntitySystem
         SubscribeLocalEvent<ParasiteComponent, InfectHostDoAfterEvent>(OnDoAfterInfestHost);
         SubscribeLocalEvent<ParasiteComponent, ParasiteEscapeDoAfterEvent>(OnDoAfterEscape);
         SubscribeLocalEvent<ParasiteComponent, ParasiteLoseHostEvent>(OnLoseHost);
+
+        SubscribeLocalEvent<ParasiteComponent, PlayerAttachedEvent>(OnPlayerAttached);
+    }
+
+    private void OnPlayerAttached(EntityUid uid, ParasiteComponent component, PlayerAttachedEvent args)
+    {
+        _audio.PlayGlobal(component.StartSound, uid);
     }
 
     public override void Update(float frameTime)
@@ -116,43 +132,51 @@ public sealed class ParasiteSystem : EntitySystem
             var previousHungerStolen = parasite.HungerStolen;
 
             StealHunger(uid, container.Owner, parasite);
+            TickEmotions(uid, container.Owner, parasite);
 
 
             if (JustReachedValue(200f, previousHungerStolen, parasite.HungerStolen))
             {
                 _popup.PopupEntity("Something feels wrong", container.Owner, container.Owner, PopupType.Medium);
+                SendMessage(container.Owner, "Something feels wrong", Color.AntiqueWhite);
             }
 
             if (JustReachedValue(400f, previousHungerStolen, parasite.HungerStolen))
             {
                 _popup.PopupEntity("I've been feeling hungrier than usual", container.Owner, container.Owner, PopupType.Medium);
+                SendMessage(container.Owner, "I've been feeling hungrier than usual", Color.AntiqueWhite);
             }
 
             if (JustReachedValue(600f, previousHungerStolen, parasite.HungerStolen))
             {
                 _popup.PopupEntity("I feel nauseous", container.Owner, container.Owner, PopupType.MediumCaution);
+                SendMessage(container.Owner, "I feel nauseous", Color.Green);
             }
 
             if (JustReachedValue(800f, previousHungerStolen, parasite.HungerStolen))
             {
                 _popup.PopupEntity("I'm so hungry all of the time", container.Owner, container.Owner, PopupType.Medium);
-                _popup.PopupEntity("You are halfway to emerging", uid, uid, PopupType.Large);
+                SendMessage(container.Owner, "I'm so hungry all of the time", Color.AntiqueWhite);
+                SendMessage(uid, "You are halfway to emerging");
             }
 
             if (JustReachedValue(1000f, previousHungerStolen, parasite.HungerStolen))
             {
                 _popup.PopupEntity("I haven't felt like myself", container.Owner, container.Owner, PopupType.Medium);
+                SendMessage(container.Owner, "I haven't felt like myself", Color.AntiqueWhite);
             }
 
             if (JustReachedValue(1200f, previousHungerStolen, parasite.HungerStolen))
             {
                 _popup.PopupEntity("Something is definitely very wrong", container.Owner, container.Owner, PopupType.MediumCaution);
+                SendMessage(container.Owner, "Something is definitely very wrong", Color.DarkRed);
             }
 
             if (JustReachedValue(1400f, previousHungerStolen, parasite.HungerStolen))
             {
                 _popup.PopupEntity("Something is inside of me", container.Owner, container.Owner, PopupType.LargeCaution);
-                _popup.PopupEntity("You will emerge soon", uid, uid, PopupType.Large);
+                SendMessage(container.Owner, "Something is inside of me", Color.DarkRed);
+                SendMessage(uid, "You will emerge soon");
             }
 
             if (Between(parasite.HungerStolen, 1400f, 1450f))
@@ -171,7 +195,7 @@ public sealed class ParasiteSystem : EntitySystem
 
             if (Between(parasite.HungerStolen, 1400f, 1500))
             {
-                _bloodstream.TryModifyBleedAmount(container.Owner, 10f);
+                _bloodstream.TryModifyBleedAmount(container.Owner, 0.5f);
             }
 
             if (parasite.HungerStolen >= 1500)
@@ -231,13 +255,89 @@ public sealed class ParasiteSystem : EntitySystem
         if (_hunger.GetHunger(hostHungerComp) < 10.0f)
         {
             ThoughtWithProbability("You feel extremely hungry", hostUid, 0.1f);
-            parasite.HungerStolen += 0.1f;
+            if (_random.Prob(0.01f))
+            {
+                _popup.PopupEntity("You NEED food", hostUid, hostUid, PopupType.MediumCaution);
+                _stun.TryParalyze(hostUid, TimeSpan.FromSeconds(2f), false);
+            }
+
+            
+            parasite.HungerStolen += 0.5f;
             return;
         }
 
         _hunger.ModifyHunger(hostUid, -1.0f, hostHungerComp);
         _hunger.ModifyHunger(parasiteUid, 1.0f, parasiteHungerComp);
         parasite.HungerStolen += 1.0f;
+    }
+
+    private void TickEmotions(EntityUid parasiteUid, EntityUid hostUid, ParasiteComponent parasite)
+    {
+        if (_timing.CurTime > parasite.AffectEmotionUntil)
+        {
+            if (parasite.NotifiedEndEmotion)
+                return;
+
+            switch (parasite.CurrentEmotion)
+            {
+                case Emotion.Anger:
+                    SendMessage(hostUid, "The irrational anger fades", Color.White);
+                    break;
+                case Emotion.Fear:
+                    SendMessage(hostUid, "The irrational fear fades", Color.White);
+                    break;
+                case Emotion.Bliss:
+                    SendMessage(hostUid, "The irrational bliss fades", Color.White);
+                    break;
+                case Emotion.Despair:
+                    SendMessage(hostUid, "The irrational despair fades", Color.White);
+                    break;
+                case Emotion.Disgust:
+                    SendMessage(hostUid, "The irrational disgust fades", Color.White);
+                    break;
+                case Emotion.Emptiness:
+                    SendMessage(hostUid, "The irrational emptiness fades", Color.White);
+                    break;
+                case Emotion.Confusion:
+                    SendMessage(hostUid, "The irrational confusion fades", Color.White);
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+
+            parasite.NotifiedEndEmotion = true;
+
+            return;
+        }
+
+        ThoughtWithProbability(_random.Pick(parasite._emotionThoughts[parasite.CurrentEmotion]), hostUid, 0.05f, PopupType.Medium);
+
+        switch (parasite.CurrentEmotion)
+        {
+            case Emotion.Anger:
+                EmoteWithProbability(hostUid, parasite.ScreamEmote, 0.05f);
+                break;
+            case Emotion.Fear:
+                EmoteWithProbability(hostUid, parasite.ScreamEmote, 0.05f);
+                break;
+            case Emotion.Bliss:
+                break;
+            case Emotion.Despair:
+                EmoteWithProbability(hostUid, parasite.CryingEmote, 0.05f);
+                break;
+            case Emotion.Disgust:
+                if (_random.Prob(0.05f))
+                {
+                    _vomit.Vomit(hostUid, -10, -10);
+                }
+                break;
+            case Emotion.Emptiness:
+                break;
+            case Emotion.Confusion:
+                break;
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
     }
 
     private void OnComponentRemove(EntityUid uid, ParasiteComponent component, ComponentRemove args)
@@ -316,44 +416,105 @@ public sealed class ParasiteSystem : EntitySystem
 
     private void OnAnger(EntityUid uid, ParasiteComponent component, ParasiteAffectEmotionAngerActionEvent args)
     {
-        SwitchEmotion(uid, component, Emotion.Anger);
+        if (!_container.TryGetContainingContainer(uid, out var container))
+            return;
+
+        _popup.PopupEntity("You suddenly feel extremely angry", container.Owner, container.Owner, PopupType.Large);
+        SendMessage(container.Owner, "You suddenly feel extremely angry. Everything pisses you off, and every word said is a slight.", Color.Red);
+
+        SwitchEmotion(component, Emotion.Anger);
     }
 
     private void OnFear(EntityUid uid, ParasiteComponent component, ParasiteAffectEmotionFearActionEvent args)
     {
-        SwitchEmotion(uid, component, Emotion.Fear);
+        if (!_container.TryGetContainingContainer(uid, out var container))
+            return;
+
+        _popup.PopupEntity("You suddenly feel existential fear", container.Owner, container.Owner, PopupType.Large);
+        SendMessage(container.Owner, "You suddenly feel an existential fear sink in. Everyone and everything is out to hurt you.", Color.DarkSlateBlue);
+
+        SwitchEmotion(component, Emotion.Fear);
     }
 
     private void OnBliss(EntityUid uid, ParasiteComponent component, ParasiteAffectEmotionBlissActionEvent args)
     {
-        SwitchEmotion(uid, component, Emotion.Bliss);
+        if (!_container.TryGetContainingContainer(uid, out var container))
+            return;
+
+        _popup.PopupEntity("You suddenly feel completely at peace", container.Owner, container.Owner, PopupType.Large);
+        SendMessage(container.Owner, "You suddenly feel completely at peace. You are drifting on a soft cloud, and no amount of sorrows can pull you from it.", Color.Aqua);
+
+        SwitchEmotion(component, Emotion.Bliss);
     }
 
     private void OnDespair(EntityUid uid, ParasiteComponent component, ParasiteAffectEmotionDespairActionEvent args)
     {
-        SwitchEmotion(uid, component, Emotion.Despair);
+        if (!_container.TryGetContainingContainer(uid, out var container))
+            return;
+
+        _popup.PopupEntity("You suddenly feel complete and utter despair", container.Owner, container.Owner, PopupType.Large);
+        SendMessage(container.Owner, "You feel a terrible sadness sink in. Everything in the world is wrong and you're the reason it's wrong.", Color.Blue);
+
+        SwitchEmotion(component, Emotion.Despair);
     }
 
     private void OnDisgust(EntityUid uid, ParasiteComponent component, ParasiteAffectEmotionDisgustActionEvent args)
     {
-        SwitchEmotion(uid, component, Emotion.Disgust);
+        if (!_container.TryGetContainingContainer(uid, out var container))
+            return;
+
+        _popup.PopupEntity("You suddenly feel completely disgusted", container.Owner, container.Owner, PopupType.Large);
+        SendMessage(container.Owner, "You suddenly feel completely disgusted with everyone and everything. It feels as though the whole world is writhing like one, big organism.", Color.DarkGreen);
+
+        SwitchEmotion(component, Emotion.Disgust);
     }
 
     private void OnEmptiness(EntityUid uid, ParasiteComponent component, ParasiteAffectEmotionEmptinessActionEvent args)
     {
-        SwitchEmotion(uid, component, Emotion.Emptiness);
+        if (!_container.TryGetContainingContainer(uid, out var container))
+            return;
+
+        _popup.PopupEntity("You suddenly feel completely empty", container.Owner, container.Owner, PopupType.Large);
+        SendMessage(container.Owner, "You suddenly feel completely empty. It is hard to feel much of anything.", Color.White);
+
+        SwitchEmotion(component, Emotion.Emptiness);
     }
 
     private void OnConfusion(EntityUid uid, ParasiteComponent component, ParasiteAffectEmotionConfusionActionEvent args)
     {
-        _chat.TrySendInGameOOCMessage(uid, "You suddenly feel completely and utterly confused", InGameOOCChatType.Looc, false);
-        SwitchEmotion(uid, component, Emotion.Confusion);
+        if (!_container.TryGetContainingContainer(uid, out var container))
+            return;
+
+        _popup.PopupEntity("You suddenly feel completely confused", container.Owner, container.Owner, PopupType.Large);
+        SendMessage(container.Owner, "You suddenly feel completely and utterly confused.", Color.BetterViolet);
+
+        SwitchEmotion(component, Emotion.Confusion);
     }
 
-    private void SwitchEmotion(EntityUid uid, ParasiteComponent component, Emotion newEmotion)
+    private void SendMessage(EntityUid uid, string message, Color? color = null)
+    {
+        if (!_mind.TryGetMind(uid, out _, out var mind))
+            return;
+
+
+        if (!_player.TryGetSessionById(mind.UserId, out var session))
+            return;
+
+        _chatManager.ChatMessageToOne(ChatChannel.Notifications, message, message, default, false, session.Channel, color);
+    }
+
+    private void SwitchEmotion(ParasiteComponent component, Emotion newEmotion)
     {
         component.CurrentEmotion = newEmotion;
-        component.AffectEmotionUntil = _timing.CurTime + TimeSpan.FromSeconds(50);
+        component.AffectEmotionUntil = _timing.CurTime + component.AffectEmotionTime;
+        component.NotifiedEndEmotion = false;
+        _actions.SetCooldown(component.AngerActionEntity, component.AffectEmotionCooldown);
+        _actions.SetCooldown(component.FearActionEntity, component.AffectEmotionCooldown);
+        _actions.SetCooldown(component.BlissActionEntity, component.AffectEmotionCooldown);
+        _actions.SetCooldown(component.DespairActionEntity, component.AffectEmotionCooldown);
+        _actions.SetCooldown(component.DisgustActionEntity, component.AffectEmotionCooldown);
+        _actions.SetCooldown(component.EmptinessActionEntity, component.AffectEmotionCooldown);
+        _actions.SetCooldown(component.ConfusionActionEntity, component.AffectEmotionCooldown);
     }
 
     #endregion
@@ -392,6 +553,7 @@ public sealed class ParasiteSystem : EntitySystem
 
         _popup.PopupEntity("You worm your way into your new host", uid, uid);
         _actions.RemoveAction(component.InfectHostActionEntity);
+        _actions.AddAction(uid, ref component.MakeDrunkActionEntity, component.MakeDrunkAction);
         _actions.AddAction(uid, ref component.EscapeActionEntity, component.EscapeAction);
         _actions.AddAction(uid, ref component.AngerActionEntity, component.AngerAction);
         _actions.AddAction(uid, ref component.FearActionEntity, component.FearAction);
@@ -422,6 +584,7 @@ public sealed class ParasiteSystem : EntitySystem
     {
         _actions.AddAction(uid, ref component.InfectHostActionEntity, component.InfectHostAction);
         _actions.RemoveAction(component.EscapeActionEntity);
+        _actions.RemoveAction(component.MakeDrunkActionEntity);
         _actions.RemoveAction(component.AngerActionEntity);
         _actions.RemoveAction(component.FearActionEntity);
         _actions.RemoveAction(component.BlissActionEntity);
